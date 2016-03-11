d4d316b62920adbc9b96dc3e1c2eaf4fbe625b59
Andrew Boss <info@smartdeveloping.com>
2016-03-11 00:52:00 +0000

    Set to enable static libs

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 ( git clone https://github.com/bitcoin/bitcoin.git /tmp/bitcoin && \
   cd /tmp/bitcoin && \
   ./autogen.sh && \
-  ./configure --disable-shared && \
+  ./configure --disable-shared --enable-static && \
   make &&
   mv /tmp/bitcoin /bitcoin-out ) || (echo "Build failed!" && exit 1)
